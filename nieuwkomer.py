85dbc95c3ec593292eaa7201bbd620dae8141716
Bas Hofland <33237@ma-web.nl>
2021-10-27 10:21:05 +0200

    eerste commit

---
+++ nieuwkomer.py
@@ -0,0 +1,32 @@
+def vraag1():
+    print("Je woont in een dorpje, er is al een tijdje oorlog maar in jouw dorp stortten nu veel gebouwen in dus je moet vertrekken. Je zoekt een uitweg, bij wie zoek je die uitweg? A: Anderen die een uitweg zoekken. B: Mensen die zeggen een uitweg voor je te hebben.")
+    vraag1 = input("")
+
+if vraag1 == "A":
+    print(test)
+elif vraag1 == "B":
+    print("B: De mesnen die zeggen dat ze je zouden helpen geven je een kezue tussen vliegtuig, auto en boot. wat doe je? A: je kiest een optie en kijkt wat er gebeurd. B: je vertrouwd het niet en gaat alsnog met de andere mee ")
+else:
+    print("Alleen hoofdletter A of B")
+    
+def vraag2():
+    print("")
+    vraag2 = input("")
+
+if vraag2 == "A":
+    print(test)
+elif vraag2 == "B":
+    print(test)
+else:
+    print("Alleen hoofdletter A of B")
+
+def vraag3():
+    print("")
+    vraag3 = input("")
+
+if vraag3 == "A":
+    print(test)
+elif vraag3 == "B":
+    print(test)
+else:
+    print("Alleen hoofdletter A of B")
